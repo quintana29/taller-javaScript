76abf31aee1ce2bc7ca41b88935ea27898eccae9
quintana29 <jhonatanquintana29@gmail.com>
2022-07-19 12:28:55 -0500

    Agregando la bola, dandole funcionalidad a las barras

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ class Board{
     }
       
     get elements(){
-        let elements = this.bars;
-        return elements;
+       
+        var elements=this.bars;
+        elements.push(this.ball);
+            return elements;
+     
     }
   
 }
@@ -25,14 +28,27 @@ class BoardView{
         this.ctx=canvas.getContext("2d");
     }
     draw =  function(){
-        console.log("jjjjjj")
+       
         for(let i= this.board.elements.length-1; i>=0; i--) {
                 let el=this.board.elements[i];
                 draw2(this.ctx,el);
             }
     }
+    
+
+    clean(){
+        this.ctx.clearRect(0,0,this.board.width,this.board.height);
+    }
+    play(){
+      
+            this.clean();
+            this.draw();
+            
+    
+        
+    }
 }
-window.addEventListener("load",main);
+window.addEventListener("load",controller);
 
 document.addEventListener("keydown",function(ev){
     console.log(ev.keyCode);
@@ -70,29 +86,54 @@ class Bar{
     }
     
 }
+class Ball{
+    constructor(x,y,radius,board){
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.speed_y=0;
+        this.speed_x=3;
+        this.speed=3;
+        this.board = board;
+        this.kind="circle";
+        board.ball=this;
+
+    }
+}
 
 var board = new Board(800,400);
-let bar = new Bar(20,100,40,100,board);
-let bar2 = new Bar(700,100,40,100,board);
+var bar = new Bar(20,100,40,100,board);
+var bar2 = new Bar(700,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
+var ball= new Ball(350,100,10,board)
+
+window.requestAnimationFrame(controller)
 function draw2(ctx,element){
 
-    if(element!== null && element.hasOwnProperty("kind")){      
+    //if(element!== null && element.hasOwnProperty("kind")){      
         console.log("ddddd")
         switch(element.kind){
             case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                     break;   
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break; 
                 
-        }
+        //}
     }
     }
     
  
 
-function main(){
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller)
 
 
-board_view.draw();
+
 }
