03ea7902e3ec7120f7184498c3a9bcac2bacd61b
quintana29 <jhonatanquintana29@gmail.com>
2022-07-19 12:49:56 -0500

    Dando movimiento a la bola, que esta pueda aparecer al momento de iniciar el juego

--- main.js
+++ main.js
@@ -1,136 +1,152 @@
-class Board{
-    constructor(width,height){
+class Board {
+    constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.playing=false;
-        this.game_over=false;
-        this.bars=[];
-        
+        this.game_over = false;
+        this.playing = false;
+        this.bars = [];
+
 
     }
-      
-    get elements(){
-       
-        var elements=this.bars;
+
+    get elements() {
+
+        var elements = this.bars.map(function (bar) { return bar; });
+
         elements.push(this.ball);
-            return elements;
-     
+        return elements;
+
     }
-  
+
 }
 
-class BoardView{
-    constructor(canvas,board){
+class BoardView {
+    constructor(canvas, board) {
         this.canvas = canvas;
-        this.canvas.width =board.width;
-        this.canvas.height=board.height;
-        this.board=board;
-        this.ctx=canvas.getContext("2d");
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
     }
-    draw =  function(){
-       
-        for(let i= this.board.elements.length-1; i>=0; i--) {
-                let el=this.board.elements[i];
-                draw2(this.ctx,el);
-            }
-    }
-    
+    draw = function () {
 
-    clean(){
-        this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        for (let i = this.board.elements.length - 1; i >= 0; i--) {
+            let el = this.board.elements[i];
+            draw2(this.ctx, el);
+        }
     }
-    play(){
-      
+
+
+    clean() {
+        this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+    }
+    play() {
+        if (this.board.playing) {
             this.clean();
             this.draw();
-            
-    
-        
+            this.board.ball.move();
+        }
     }
 }
-window.addEventListener("load",controller);
 
-document.addEventListener("keydown",function(ev){
-    console.log(ev.keyCode);
-    if(ev.keyCode == 38){
-      bar.up();
-   }
-   else if(ev.keyCode == 40){
-     bar.down();
-   }
-  });
+window.addEventListener("load", controller);
 
-class Bar{
-    constructor(x,y,width,height,board){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
-        this.board=board;
-        this.board.bars.push(this)
-        this.kind="rectangle";
-        this.speed=10;
-
+document.addEventListener("keydown", function (ev) {
+    if (ev.keyCode == 38) {
+        ev.preventDefault();
+        bar.up();
+    } else if (ev.keyCode == 40) {
+        ev.preventDefault();
+        bar.down();
+    } else if (ev.keyCode === 87) {
+        ev.preventDefault();
+        bar_2.up();
+    } else if (ev.keyCode === 83) {
+        ev.preventDefault();
+        bar_2.down();
+    } else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
-   
-    down(){
-        this.y+=this.speed;
+});
 
-    }
-    up(){
-        this.y-=this.speed;
-
-    }
-    toString(){
-        return "x:"+this.x+"y:"+this.y;
-    }
-    
-}
-class Ball{
-    constructor(x,y,radius,board){
-        this.x=x;
-        this.y=y;
-        this.radius=radius;
-        this.speed_y=0;
-        this.speed_x=3;
-        this.speed=3;
+class Bar {
+    constructor(x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.board = board;
-        this.kind="circle";
-        board.ball=this;
+        this.board.bars.push(this)
+        this.kind = "rectangle";
+        this.speed = 10;
 
+    }
+
+    down() {
+        this.y += this.speed;
+
+    }
+    up() {
+        this.y -= this.speed;
+
+    }
+    toString() {
+        return "x:" + this.x + "y:" + this.y;
+    }
+
+}
+class Ball {
+    constructor(x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.speed = 3;
+        this.board = board;
+        this.kind = "circle";
+        this.direction = 1;
+        board.ball = this;
+
+    }
+
+    move() {
+        this.x += (this.speed_x * this.direction)
+        this.y += (this.speed_y);
     }
 }
 
-var board = new Board(800,400);
-var bar = new Bar(20,100,40,100,board);
-var bar2 = new Bar(700,100,40,100,board);
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar2 = new Bar(700, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
-var board_view = new BoardView(canvas,board);
-var ball= new Ball(350,100,10,board)
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board)
 
-window.requestAnimationFrame(controller)
-function draw2(ctx,element){
+window.requestAnimationFrame(controller);
+board_view.draw();
+function draw2(ctx, element) {
 
     //if(element!== null && element.hasOwnProperty("kind")){      
-        console.log("ddddd")
-        switch(element.kind){
-            case "rectangle":
-                ctx.fillRect(element.x, element.y, element.width, element.height);
-                break;
-            case "circle":
-                ctx.beginPath();
-                ctx.arc(element.x, element.y, element.radius, 0, 7);
-                ctx.fill();
-                ctx.closePath();
-                break; 
-                
+    console.log("ddddd")
+    switch (element.kind) {
+        case "rectangle":
+            ctx.fillRect(element.x, element.y, element.width, element.height);
+            break;
+        case "circle":
+            ctx.beginPath();
+            ctx.arc(element.x, element.y, element.radius, 0, 7);
+            ctx.fill();
+            ctx.closePath();
+            break;
+
         //}
     }
-    }
-    
- 
+}
 
-function controller(){
+
+function controller() {
     board_view.play();
     window.requestAnimationFrame(controller)
 
